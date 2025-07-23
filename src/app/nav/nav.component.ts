33da20f130df7f7f13cdc6add8b7f60d5400b9a3
prateek790 <33649072+prateek790@users.noreply.github.com>
2025-07-23 23:26:31 +0530

    Angular Template Forms

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+ model: any = {}
 
+ login() {
+  console.log(this.model);
+ }
 }
