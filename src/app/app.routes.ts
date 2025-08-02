d0096dcc506660910ff562b13e403a714961f715
prateek790 <33649072+prateek790@users.noreply.github.com>
2025-08-02 23:49:11 +0530

    adding member, list and message  component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { MemberListComponent } from './features/members/member-list/member-list.component';
+import { MemberDetailedComponent } from './features/members/member-detailed/member-detailed.component';
+import { MessagesComponent } from './features/messages/messages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'members', component: MemberListComponent},
+    {path : 'members/:id', component: MemberDetailedComponent},
+    {path: 'list', component: MemberListComponent},
+    {path: 'messages', component: MessagesComponent},
+    {path: '**', component: HomeComponent}
+];
+
+ 
